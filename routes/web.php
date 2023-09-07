a8a34bc7a450cda00f374cc8a4ee5b18dc2e1e8e
Martarelli <renanmartarelli@gmail.com>
2023-09-07 18:15:32 -0300

    rotas para cadastro

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ClienteController::class, 'index']);
+
+Route::get('/create', [ClienteController::class, 'create']);
+Route::post('/create/store', [ClienteController::class, 'store']);
